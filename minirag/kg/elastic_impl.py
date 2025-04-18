a36c530c2ce51ed68d2ba4ca76f8e11dd4c40b89
Waveletter <56689975+Waveletter@users.noreply.github.com>
2025-04-18 17:00:33 +0300

    Added connection testing and plugs for future classes

--- minirag/kg/elastic_impl.py
+++ minirag/kg/elastic_impl.py
@@ -5,6 +5,8 @@ import os
 import time
 from dataclasses import dataclass
 from typing import Union, List, Dict, Set, Any, Tuple
+
+import elasticsearch
 import numpy as np
 
 import sys
@@ -16,8 +18,8 @@ from tenacity import (
     wait_exponential,
 )
 
-from ..utils import logger
-from ..base import (
+from minirag.utils import logger
+from minirag.base import (
     BaseKVStorage,
     BaseVectorStorage,
     DocStatusStorage,
@@ -30,3 +32,48 @@ if sys.platform.startswith("win"):
     import asyncio.windows_events
 
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+@dataclass
+class ESKVStorage(BaseKVStorage):
+    pass
+
+@dataclass
+class ESVectorStorage(BaseVectorStorage):
+    pass
+
+@dataclass
+class ESDocStatusStorage(DocStatusStorage):
+    pass
+
+class ESGraphQueryException(Exception):
+    pass
+
+@dataclass
+class ESGraphStorage(BaseGraphStorage):
+    pass
+
+from info import ES_Info
+
+def main():
+    print(ES_Info.host.value, ES_Info.credentials.value)
+    es = elasticsearch.Elasticsearch(
+        ES_Info.host.value,
+        verify_certs=False,
+        ssl_show_warn=False,
+        api_key=ES_Info.api_key.value
+        #basic_auth=ES_Info.credentials.value
+    )
+
+    if es.ping():
+        print("Connection successful")
+    else:
+        print("No connection")
+
+    es.info()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        print(ex)
